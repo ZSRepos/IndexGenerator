6fa6e2a3bc52396d6d78620348b490d185eb65ba
Gao Liu <liugaocn@outlook.com>
2022-02-19 10:45:48 +0800

    Add project files.

---
+++ GetAllFileAndFolderNames.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexGenerator
+{
+    internal class GetAllFileAndFolderNames
+    {
+        /// <summary>
+        /// 获得指定路径下所有文件名
+        /// </summary>
+        /// <param name="sw">文件写入流</param>
+        /// <param name="path">文件写入流</param>
+        /// <param name="indent">输出时的缩进量</param>
+        public static void getFileName(StreamWriter sw, string path, string parentPathOfRoot)
+        {
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (FileInfo f in root.GetFiles())
+            {
+                //for (int i = 0; i < indent; i++)
+                //{
+                //    sw.Write("  ");
+                //}
+                if(f.Name.ToLower().EndsWith(".html") && !f.Name.ToLower().Contains("index"))
+                {
+                    sw.Write("<div><a href=\"");
+                    // sw.WriteLine(path.Split(Path.DirectorySeparatorChar)[^1] + "/" + f.Name);
+                    sw.Write((f.DirectoryName + "\\" + f.Name).Replace(parentPathOfRoot + "\\", ""));
+                    //Console.WriteLine(f.Name);
+                    sw.WriteLine("\">" + Path.GetFileNameWithoutExtension(f.FullName) +"</a></div>");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获得指定路径下所有子目录名
+        /// </summary>
+        /// <param name="sw">文件写入流</param>
+        /// <param name="path">文件夹路径</param>
+        /// <param name="indent">输出时的缩进量</param>
+        public static void getDirectory(StreamWriter sw, string path, string parentPathOfRoot)
+        {
+            getFileName(sw, path, parentPathOfRoot);
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (DirectoryInfo d in root.GetDirectories())
+            {
+                //for (int i = 0; i < indent; i++)
+                //{
+                //    sw.Write("  ");
+                //}
+                // sw.WriteLine("文件夹：" + d.Name);
+                if(!d.Name.ToLower().Contains("00模板"))
+                {
+                    getDirectory(sw, d.FullName, parentPathOfRoot);
+                }
+                // sw.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using IndexGenerator;
+
+namespace Program
+{
+    public class Generator
+    {
+        static void Main(string[] vs)
+        {
+            //获取当前程序所在的文件路径
+            String rootPath = @"D:\Programming Projects\web projects\ZSClinic\Client\Pages";
+            string parentPath = Directory.GetParent(rootPath).FullName;
+            string fileName = @"fileList.html";
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            StreamWriter sw = new StreamWriter(fileName, true);
+            // sw.WriteLine("根目录：" + rootPath);
+           GetAllFileAndFolderNames.getDirectory(sw, rootPath, parentPath);
+            sw.Flush();
+            sw.Dispose();
+        }
+    }
+}
